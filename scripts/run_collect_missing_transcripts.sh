09b870d3f92eabfbb45f2c9cda41cef8a29511bc
CarolineViolot <43467129+CarolineViolot@users.noreply.github.com>
2026-07-22 14:43:28 +0200

    adapted code for cluster

--- scripts/run_collect_missing_transcripts.sh
+++ scripts/run_collect_missing_transcripts.sh
@@ -1,6 +1,18 @@
 #!/bin/bash -l
 
-python collect_missing_transcripts.py \
+#SBATCH --job-name transcript_collect
+#SBATCH --error logs/%j.error
+#SBATCH --output logs/%j.out
+
+#SBATCH --partition cpu
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 10G
+#SBATCH --time 12:00:00
+#SBATCH --export NONE
+#SBATCH --mail-type END,FAIL
+#SBATCH --mail-user caroline.violot@unil.ch
+
+python scripts/collect_missing_transcripts.py \
   --platform "youtube" \
   --year "2022" \
   --channel_type "news" \
