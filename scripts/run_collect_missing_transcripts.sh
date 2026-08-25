041ca07570c73664b1141055e30d6aab69e80abb
CarolineViolot <43467129+CarolineViolot@users.noreply.github.com>
2026-08-25 05:02:35 +0200

    deno path

--- scripts/run_collect_missing_transcripts.sh
+++ scripts/run_collect_missing_transcripts.sh
@@ -23,7 +23,7 @@ source $WORK_PATH/pythonenv_new/bin/activate
 cd /work/FAC/HEC/DESI/mhumber6/youtubeshorts/VideosRepurposing
 export PYTHONPATH="$PWD"
 
-export PATH=/scratch/cviolot/French-Politicians/deno-bin:$PATH
+export PATH=/scratch/cviolot/French-Politicians/deno/bin:$PATH
 
 python scripts/collect_missing_transcripts.py \
   --platform "youtube" \
